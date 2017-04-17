3d9d2abc1c25e6fd2430f2e16fd42405971a5e0a
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-04-17 15:18:12 -0300

    refactor(Batalha): reduzir if duplicado

--- cavaleiros-aula1/Batalha.java
+++ cavaleiros-aula1/Batalha.java
@@ -1,7 +1,6 @@
-
 public class Batalha
 {
-    private Saint saint1, saint2;
+    private Saint saint1, saint2; 
    
     public Batalha(Saint saint1, Saint saint2)
     {
@@ -9,26 +8,15 @@ public class Batalha
        this.saint2 = saint2;
     }
     
-    /*public void iniciar(){
-        if(saint1.getValorArmadura()>saint2.getValorArmadura()){
-           saint2.perderVida(10);
-        } else if(saint1.getValorArmadura()<saint2.getValorArmadura()){
-                  saint1.perderVida(10);
-               } else saint2.perderVida(10);
-        
-        if(saint1.getVida()<100) saint2.perderVida(10);
-        else saint1.perderVida(10);
-        
-    }*/
-    
     public void iniciar(){
         int valor1 = this.saint1.getValorArmadura();
         int valor2 = this.saint2.getValorArmadura();
+        final double dano = 10;
         
-        if(valor1> valor2){
-            this.saint2.perderVida(10);
-        } else if (valor1 == valor2){
-            this.saint2.perderVida(10);
-        } else this.saint1.perderVida(10);
+        if(valor1>= valor2){
+            this.saint2.perderVida(dano);
+        } else{ 
+             this.saint1.perderVida(dano);
+               }
     }
 }
