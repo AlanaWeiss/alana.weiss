84d470604fdd1f398db1f62556368b6a80caf348
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-04-17 17:19:31 -0300

    feat(SilverSaint): implentar classe para cavaleiros de prata

--- cavaleiros-aula1/SaintTest.java
+++ cavaleiros-aula1/SaintTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 public class SaintTest{
     @Test
-    public void vestirArmaduraDeixaArmaduraVEstida(){
+    public void vestirArmaduraDeixaArmaduraVEstida()throws Exception {
        //AAA
        //1. Arrange - Montagem dos dados de teste
        Armadura escorpiao = new Armadura("Escorpião", Categoria.OURO);
@@ -21,84 +21,84 @@ public class SaintTest{
     }
     
     @Test
-    public void naoVestirArmaduraDeixaArmaduraNaoVestida(){
+    public void naoVestirArmaduraDeixaArmaduraNaoVestida()throws Exception {
         Saint hyoga = new Saint("Hyoga", new Armadura("Cisne", Categoria.BRONZE));
         assertEquals(false, hyoga.getArmaduraVestida());
     }
 
     @Test
-    public void aoCriarSaintGeneroENaoInformado(){
+    public void aoCriarSaintGeneroENaoInformado()throws Exception {
         Saint shaka = new Saint("Shaka", new Armadura("Virgem", Categoria.OURO));
         assertEquals(Gênero.NAO_INFORMADO, shaka.getGenero());
     }
     
     @Test 
-    public void aoCriarSaintStatusEVivo(){
+    public void aoCriarSaintStatusEVivo()throws Exception {
         Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.BRONZE));
         assertEquals(Status.VIVO, dante.getStatus());
     }
     
     @Test
-    public void saintsNascemCom100DeVida(){
+    public void saintsNascemCom100DeVida()throws Exception {
         Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.BRONZE));
         assertEquals(100.0, dante.getVida(),0.01);
     }
     
     @Test
-    public void deveSerPossivelAlterarGenero(){
+    public void deveSerPossivelAlterarGenero()throws Exception {
         Saint jabu = new Saint("Dante", new Armadura("Cisne", Categoria.BRONZE));
         jabu.setGenero(Gênero.MASCULINO);
         assertEquals(Gênero.MASCULINO, jabu.getGenero());
     }
     
     @Test
-    public void perderDanoComValor10(){
+    public void perderDanoComValor10()throws Exception {
         Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.BRONZE));
         dante.perderVida(10);
         assertEquals(90,dante.getVida(),0.01);
     }
     
     @Test
-    public void perderDanoComValor100(){
+    public void perderDanoComValor100()throws Exception {
         Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.BRONZE));
         dante.perderVida(100);
         assertEquals(0,dante.getVida(),0.01);
     }
     
     @Test
-    public void perderDanoComValor1000(){
+    public void perderDanoComValor1000()throws Exception {
         Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.BRONZE));
         dante.perderVida(1000);
         assertEquals(-900,dante.getVida(),0.01);
     }
     
      @Test
-    public void perderDanoComValorMenos1000(){
+    public void perderDanoComValorMenos1000()throws Exception {
         Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.BRONZE));
         dante.perderVida(-1000);
         assertEquals(1100,dante.getVida(),0.01);
     }
     
     @Test
-    public void criarSaintNasceCom5SentidosDespertados(){
-        Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.BRONZE));
+    public void criarSaintNasceCom5SentidosDespertados()throws Exception {
+        BronzeSaint dante = new BronzeSaint("Dante", new Armadura("Cisne", Categoria.BRONZE));
         assertEquals(5, dante.getQtdSentidosDespertados());
     }
     
     @Test
-    public void criarSaintOuroNasceCom7Sentidos(){
-        Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.OURO));
+    public void criarSaintOuroNasceCom7Sentidos()throws Exception {
+        GoldSaint dante = new GoldSaint("Dante", new Armadura("Virgem", Categoria.OURO));
         assertEquals(7, dante.getQtdSentidosDespertados());
     }
     
     @Test
-    public void criarSaintPrataNasceCom6Sentidos(){
-        Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.PRATA));
+    public void criarSaintPrataNasceCom6Sentidos()throws Exception {
+        SilverSaint dante = new SilverSaint("Dante", new Armadura("Cisne", Categoria.PRATA));
         assertEquals(6, dante.getQtdSentidosDespertados());
     }
     @Test
-    public void criarSaintBronzeNasceCom5Sentidos(){
-        Saint dante = new Saint("Dante", new Armadura("Cisne", Categoria.BRONZE));
+    public void criarSaintBronzeNasceCom5Sentidos()throws Exception {
+        BronzeSaint dante = new BronzeSaint("Dante", new Armadura("Cisne", Categoria.BRONZE));
         assertEquals(5, dante.getQtdSentidosDespertados());
     }
 }
