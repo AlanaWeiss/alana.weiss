895542cc1e8480919ceeb0a6eef47458a1d1e8e7
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-04-18 19:46:52 -0300

    feat(ListaSaints): adicionar classe que lista saints com função para adicionar os saints

--- cavaleiros-aula1/Constelacao.java
+++ cavaleiros-aula1/Constelacao.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Constelacao {
     private String nome;
     //private Golpe[] golpes = new Golpe[3];
-	ArrayList<Golpe> golpes = new ArrayList<>();
+    ArrayList<Golpe> golpes = new ArrayList<>();
     private int ultimaPosicaoPreenchida = 0;
  
     public Constelacao(String nome){
@@ -11,17 +11,8 @@ public class Constelacao {
     }
     
     public void adicionarGolpe(Golpe golpe){
-        /*for (int i = 0; i< this.golpes.length; i++){
-            Golpe golpeAtual = this.golpes[i];
-            if(golpeAtual == null){ 
-                this.golpes[i]= golpe;
-                break; //quebra o for
-                //continue quebra a execução atual
-                //return quebra o metodo inteiro
-            }
-        }*/
-       // golpes[ultimaPosicaoPreenchida++]=golpe;
-		golpes.add(golpe);
+        golpes.add(golpe); //golpeS = array
+                           //golpE = parametro        
     }
     
     public ArrayList getGolpes(){
---
+++ cavaleiros-aula1/ListaSaints.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class ListaSaints{
+    ArrayList<Saint> saints = new ArrayList<>();
+    
+    public void adicionar(Saint saint){
+        saints.add(saint);
+    }
+    
+}
