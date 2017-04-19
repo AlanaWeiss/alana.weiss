6a329c12147546c74540742d4808f496e4a5f483
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-04-18 21:02:13 -0300

    feat(ListaSaint): implementa metodo get e remove

--- cavaleiros-aula1/ListaSaints.java
+++ cavaleiros-aula1/ListaSaints.java
@@ -1,9 +1,27 @@
 import java.util.ArrayList;
 public class ListaSaints{
     ArrayList<Saint> saints = new ArrayList<>();
+    private int indice;
     
     public void adicionar(Saint saint){
         saints.add(saint);
     }
     
+    public void adicionarSaint(Saint saint){
+        saints.add(saint); //golpeS = array
+                           //golpE = parametro        
+    }
+     
+    public ArrayList todos(){
+        return saints;
+    }
+    
+    public Saint get(int indice){
+        return saints.get(indice);
+    }
+       
+    public void remover(Saint saint){
+        int i = saints.indexOf(saint);
+        saints.remove(i);
+    }
 }
