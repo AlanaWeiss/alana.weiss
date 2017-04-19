a63e89dafd64d02a70c7167e69b4a4edf919480d
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-04-19 17:25:39 -0300

    test(ListaSaint): adicionar teste para ordenar ascendente

--- cavaleiros-aula1/ListaSaints.java
+++ cavaleiros-aula1/ListaSaints.java
@@ -100,19 +100,20 @@ public class ListaSaints{
     public void ordenar(TipoOrdenacao ordenacao){
         if(TipoOrdenacao.ASCENDENTE.equals(ordenacao)){
             boolean posicoesSendoTrocadas;
-        do {
-            posicoesSendoTrocadas = false;
-            for (int i = 0; i < this.saints.size() - 1; i++) {
-                Saint atual = this.saints.get(i);
-                Saint proximo = this.saints.get(i + 1);
-                boolean precisaTrocar = atual.getVida() > proximo.getVida();
-                if (precisaTrocar) {
-                    this.saints.set(i, proximo);
-                    this.saints.set(i + 1, atual);
-                    posicoesSendoTrocadas = true;
+            do {
+                posicoesSendoTrocadas = false;
+                for (int i = 0; i < this.saints.size() - 1; i++) {
+                    Saint atual = this.saints.get(i);
+                    Saint proximo = this.saints.get(i + 1);
+                    boolean precisaTrocar = atual.getVida() > proximo.getVida();
+                    if (precisaTrocar) {
+                        this.saints.set(i, proximo);
+                        this.saints.set(i + 1, atual);
+                        posicoesSendoTrocadas = true;
+                    }
                 }
+            } while (posicoesSendoTrocadas);   
             }
-        } while (posicoesSendoTrocadas);   
-        }
     }
+    
 }
