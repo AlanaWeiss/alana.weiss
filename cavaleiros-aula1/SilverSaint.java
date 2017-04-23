3bf8a99befaf1ab71f9af458ba63c8d0e0bf58e6
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-04-23 17:29:09 -0300

    fix(BronzeSaint): corrigir construtor com categoria errada

--- cavaleiros-aula1/SilverSaint.java
+++ cavaleiros-aula1/SilverSaint.java
@@ -2,7 +2,7 @@ public class SilverSaint extends Saint{
     
     public SilverSaint(String nome, String constelacao)throws Exception{
         super(nome, constelacao);
-        this.armadura = new Armadura(new Constelacao(constelacao), Categoria.BRONZE);
+        this.armadura = new Armadura(new Constelacao(constelacao), Categoria.PRATA);
         this.qtdSentidosDespertados = 6;
     }
 }
