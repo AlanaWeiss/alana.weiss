3335e5bab593c1b87904a3079522395aa49e2dae
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-04-24 19:49:49 -0300

    fix(Batalha): corrigir golpe inicial que nao estava tendo antes

--- cavaleiros-aula1/Batalha.java
+++ cavaleiros-aula1/Batalha.java
@@ -1,38 +1,37 @@
-public class Batalha
-{
-    private Saint saint1, saint2; 
-   
-    public Batalha(Saint saint1, Saint saint2)
-    {
-       this.saint1 = saint1;
-       this.saint2 = saint2;
+public class Batalha {
+
+    private Saint saint1, saint2;
+
+    public Batalha(Saint saint1, Saint saint2) {
+        this.saint1 = saint1;
+        this.saint2 = saint2;
     }
-    
+
     public void iniciar() throws Exception{
         int valor1 = this.saint1.getValorArmadura();
         int valor2 = this.saint2.getValorArmadura();
         final double dano = 10;
-        Saint atacaPrimeiro, atacaDepois = null;
-        Movimento movimento;
+        Saint saintEmAcao = null;
 
         if (valor1 >= valor2) {
-            atacaPrimeiro = this.saint1;
-            atacaDepois = this.saint2;
+            saintEmAcao = this.saint1;
             this.saint2.perderVida(dano);
         } else {
-            atacaPrimeiro = this.saint2;
-            atacaDepois = this.saint1;
+            saintEmAcao = this.saint2;
             this.saint1.perderVida(dano);
         }
-        boolean estaoVivos = true;
-        while (estaoVivos){
-            movimento = atacaPrimeiro.getProximoMovimento();
-            movimento.executar();
-            if(atacaDepois.getStatus() != Status.MORTO){
-                movimento = atacaDepois.getProximoMovimento();
-                movimento.executar();
-            }
-            estaoVivos = this.saint1.getStatus() != Status.MORTO && this.saint2.getStatus() != Status.MORTO;
+
+        boolean nenhumMorto = true;
+        while (nenhumMorto) {
+            // 1. definindo quem vai atuar no round
+            saintEmAcao = saintEmAcao == this.saint1 ? this.saint2 : this.saint1;
+            // 2. executando próximo movimento
+            Movimento proximoMovimento = saintEmAcao.getProximoMovimento();
+            proximoMovimento.executar();
+            // 3. verificando se a batalha acabou
+            nenhumMorto = this.saint1.getStatus() != Status.MORTO &&
+            this.saint2.getStatus() != Status.MORTO;
         }
+
     }
 }
