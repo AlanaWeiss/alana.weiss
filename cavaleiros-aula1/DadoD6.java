ae8c65df3acd4d7e0c5231f25e474215c1b9120b
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-04-26 16:02:38 -0300

    Auto stash for revert of "refactor(GoldSaint): melhorar identação"

---
+++ cavaleiros-aula1/DadoD6.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class DadoD6 implements Sorteador{
+
+        public int sortear(){
+            final Random random = new Random();
+            int min = 1, max = 6;
+            return random.nextInt(max - min + 1) + min;
+        }
+        
+}
+  
