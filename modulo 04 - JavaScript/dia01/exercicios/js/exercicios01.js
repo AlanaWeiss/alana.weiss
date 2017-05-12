71a1a3e854d6df222f4044da7ed2691f371246f3
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-05-12 17:29:10 -0300

    feat(exercicios lista 1): implementar outros metodos para resolução dos exercicios

--- modulo 04 - JavaScript/dia01/exercicios/js/exercicios01.js
+++ modulo 04 - JavaScript/dia01/exercicios/js/exercicios01.js
@@ -1,5 +1,4 @@
-console.log("Carregou");
-
+// ---------- EXERCICIO 1
 function daisyGame(petalas){
   if( petalas % 2 === 1 ){
     console.log("Love Me");
@@ -7,13 +6,17 @@ function daisyGame(petalas){
     console.log("Love Me Not");
   }
 }
+  //   function daisyGame(petalas){
+  //   //return petalas !== 0? 'Love Me' : 'Love Me Not'
+  //   return `Love Me ${ petalas % 2 !== 0 ? '' : 'Not'}`
+  // }
 
 console.log( daisyGame(1));
 console.log( daisyGame(2));
 console.log( daisyGame(3));
 console.log( daisyGame(4));
 
-//exercicio 2
+//---------exercicio 2
 
 function maiorTexto(lista) {
   var maiorTexto = " ";
@@ -23,6 +26,18 @@ function maiorTexto(lista) {
   return maiorTexto;
 }
 console.log(maiorTexto(['loucura', 'gsus', 'lalala', 'Bom dia como vai voce']));
+
+/* correção
+  function maiorTexto(textos){
+  var maiorTam = "";
+  for(le i = 0; i < textos.length; i++){
+    if(textos[i].length > maiorTam.length){
+      maiorTam = textos[i];
+    }
+  }
+  return maiorTam;
+}
+*/
 
 //exercicio 3
 
@@ -35,13 +50,17 @@ function imprime(arrayDeStrings, funcao) {
   else console.log("Jabulani");
 }
 
-function funcion(nomeInstrutor) {
+function ola(nomeInstrutor) {
   console.log("Olá querido instrutor:", nomeInstrutor);
 }
 
-console.log(imprime(["Bernardo", "Nunes", "Fabrício", "Ben-hur", "Carlos"],
- funcion))
+console.log(imprime(["Bernardo", "Nunes", "Fabrício", "Ben-hur", "Carlos"], ola))
 
+/*
+  function imprime(texto, funcao){
+  texto.forEach( (t) ) => funcao(t) --> t cada elemento do array
+}
+*/
  // ex 4
 
  function adicionar(a) {
@@ -71,6 +90,14 @@ function fiboSum(num) {
   return fiboSum-1;
 }
 console.log("FiboSum: ", fiboSum(4));
+/*
+ RECURSIVIDDADE
+    function fibonacci(n){
+    if(n===1 || n===2) return 1;
+    return(fibonacci(n-1) + fibonacci(n-2))
+  }
+
+console.log(fibonacci(4));*/
 
 //----------------- exercicio 6
 // var numericArray = [80, 9, 700];
@@ -86,6 +113,8 @@ function queroCafe(mascada, precos){
       implementaIndex++;
     }
   }
-  console.log(arrayAux.sort().toString());
+  console.log(arrayAux.sort( (a, b) => a-b ).toString());
 }
 queroCafe(3.14, [ 5.16, 2.12, 1.15, 3.11, 17.5 ])
+
+queroCafe(3.14, [ 5.16, 2.12, 1.15, 3.11, 1.5, 3.4, 88, 8.8 ])
