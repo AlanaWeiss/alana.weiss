80a0592c5cab6941f66f5df9935319b0a032b59a
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-05-17 17:34:06 -0300

    feat(ng-repeat)

--- modulo-5 angularJs/controller.js
+++ modulo-5 angularJs/controller.js
@@ -2,6 +2,24 @@ var modulo = angular.module('exercicio', []);
  
 modulo.controller('MainController', function($scope){
  
-    $scope.pokemon = {nome:'Pikachu'};
- 
-})
+    $scope.pokemon = {nome:'Pikachu', tipo: 'Default'};
+});
+
+modulo.controller('ex02', function($scope) {
+	$scope.pokemons = [
+        {
+            nome: 'poke1',
+             tipo: 'tipo1'
+        },
+        {
+            nome: 'poke2', 
+            tipo: 'tipo2'
+        },
+        {
+            nome: 'poke3',
+            tipo: 'tipo3'},
+        {
+            nome: 'poke4', 
+            tipo: 'tipo4'
+        }];
+});
