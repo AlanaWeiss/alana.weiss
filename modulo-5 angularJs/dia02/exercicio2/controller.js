c998f24ac37686f94d0453785181bbb2aa29928a
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-05-18 17:14:50 -0300

    feat(Filtro customizado): implementar a criação de um filtro

---
+++ modulo-5 angularJs/dia02/exercicio2/controller.js
@@ -0,0 +1,46 @@
+var modulo = angular.module('exercicio', []);
+
+modulo.filter('mascada', function(){
+  return function(nome){
+    return nome.replace(/(nunes)/gi,' $ $1 $ ');
+  };
+});
+
+modulo.controller('instrutores', function($scope){ 
+
+
+  $scope.instrutores = [{
+    nome: 'Bernardo',
+    aula: [{
+        numero: 1,
+        nome: 'OO'
+      },
+      {
+        numero: 4,
+        nome: 'Javascript'
+      }
+    ]
+  },
+  {
+    nome: 'Nunes',
+    aula: [{
+      numero: 2,
+      nome: 'Banco de Dados I'
+    }]
+  },
+  {
+    nome: 'Pedro (PHP)',
+    aula: [{
+      numero: 3,
+      nome: 'HTML e CSS'
+    }]
+  },
+  {
+    nome: 'Zanatta',
+    aula: [{
+      numero: 5,
+      nome: 'AngularJS'
+    }]
+  }
+];
+});
