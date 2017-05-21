0000e46cb22b4a75c1e8a311c12a83202ca3c30d
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-05-21 20:41:31 -0300

    feat(Trabalho fim de semana) adicionar resolução para o trabalho proposto

--- modulo-5 angularJs/crud/controller.js
+++ modulo-5 angularJs/crud/controller.js
@@ -1,32 +1,34 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function($scope){
+    $scope.mostrarAvisoAula = false;
 
-    var aulas = [{
-      nome: 'Orientação a obj', // Obrigatório (length = min 3, max 20)
-      id: 0
-      }];
+    $scope.instrutores = [{
+    }];
 
-      $scope.novaAula = {}
+    $scope.aulas =[];
 
-    $scope.aulas = aulas;
-    function idArray(aulas) {
-      if (aulas.length < 0) {
-        return 0;
-       } else {
-         return aulas[aulas.length-1].id+1;
-      }
-    }
+    let idAula = 0;
+    let idInstrutor = 0;
+
     $scope.incluir = function () {
-      $scope.novaAula.id = idArray(aulas);
-        $scope.aulas.push(angular.copy($scope.novaAula));
-        $scope.novaAula = {}
+      if($scope.meuForm.$valid && !verificaAulaExiste($scope.novaAula.nome)){
+        $scope.novaAula.id = idAula++;
+          $scope.aulas.push(angular.copy($scope.novaAula));
+          $scope.novaAula = {}
+      } else alert("Aula já cadastada");
+      alert("Ação realizada com sucesso!");
+    }
+
+    function verificaAulaExiste(nome) {
+      return $scope.aulas.some(a => a.nome === nome);
     }
 
     $scope.excluirAula = function (index) {
       $scope.aulas.splice(index, 1);
       $scope.removerInstrutor = {};
       $scope.formExcluirAula.$setPristine();
+      alert("Ação realizada com sucesso!");
     }
 
     $scope.editarAula = function (index) {
@@ -34,5 +36,58 @@ myApp.controller('mainController', function($scope){
        if(item.id === Number(index))
          item.nome = $scope.nomeEditado;
      });
+     alert("Ação realizada com sucesso!");
+    }
+
+  //   $scope.incluirInstrutor= function(){
+  //     if($scope.formInstrutor.$valid && !verificaInstutorComMesmoNome($scope.novoInstrutor.nome) && !verificaEmail($scope.novoInstrutor.email)){
+  //     $scope.novoInstrutor.id = idInstrutor++;
+  //     $scope.instrutores.push(angular.copy($scope.novoInstrutor));
+  //     $scope.novoInstrutor = {}
+  //   } else {
+  //     $scope.mostrarAvisoNome = true;
+  //     $scope.mostrarAvisoEmail = true;
+  //   }
+  //  }
+
+  $scope.incluirInstrutor= function(){
+    if($scope.formInstrutor.$valid){
+      if(!verificaInstutorComMesmoNome($scope.novoInstrutor.nome)){
+        if (!verificaEmail($scope.novoInstrutor.email)) {
+          $scope.novoInstrutor.id = idInstrutor++;
+          if (typeof $scope.novoInstrutor.urlFoto === 'undefined') $scope.novoInstrutor.urlFoto = "https:/pbs.twimg.com/media/C8SpgFqXYAActhH.jpg";
+          $scope.instrutores.push(angular.copy($scope.novoInstrutor));
+          $scope.novoInstrutor = {}
+        } else alert("Email já está sendo utilizado");
+      } else alert("Instrutor já cadastrado");
+    }
+    alert("Ação realizada com sucesso!");
+  }
+
+   function verificaInstutorComMesmoNome(nome) {
+     return $scope.instrutores.some(a => a.nome === nome);
+   }
+
+   function verificaEmail(email) {
+     return $scope.instrutores.some(a => a.email === email);
+   }
+
+   $scope.querEditar = function (instrutor) {
+         $scope.clicouParaEditar = true;
+         $scope.instrutorEditado = instrutor;
+    }
+
+    $scope.salvarInstrutor = function(instrutorEditado){
+        let idInstrutor = instrutorEditado.id;
+        $scope.instrutores.splice(idInstrutor,1,instrutorEditado);
+        $scope.instrutorEditado = {};
+        $scope.clicouParaEditar = false;
+        alert("Ação realizada com sucesso!");
+    }
+
+    $scope.querExcluir = function (instrutor){
+        let idInstrutor = $scope.instrutores.indexOf(instrutor);
+	    $scope.instrutores.splice(idInstrutor, 1);
+      alert("Ação realizada com sucesso!");
     }
 })
