09f91e623649036275f08748953bdd007b039ef0
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-06-03 15:00:15 -0300

    feat(dotNet): renomear pasta, adicionar cadastro de usuarios

---
+++ modulo-6-dotNet/AulaTerca/EditoraCrescer/EditoraCrescer.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EditoraCrescer.Infraesturtura.Entidades;
+using EditoraCrescer.Infraesturtura.Repositorios;
+
+namespace EditoraCrescer.Api.Controllers
+{
+    [RoutePrefix("api/acessos")]
+    public class UsuarioController : ApiController
+    {
+        private UsuarioRepositorio repositorio = new UsuarioRepositorio();
+
+        private HttpResponseMessage resposta(bool sucesso, dynamic mensagens)
+        {
+            if (sucesso)
+                return Request.CreateResponse(HttpStatusCode.OK, new { mensagens = mensagens });
+
+            return Request.CreateResponse(HttpStatusCode.BadRequest, new { mensagens = mensagens });
+        }
+
+        [HttpPost, Route("registrar")]
+        public HttpResponseMessage cadastrarUsuario(Usuario usuario)
+        {
+            if (repositorio.buscar(usuario.Email) == null)
+            {
+                if (usuario.Validar())
+                    repositorio.cadastrar(usuario);
+
+                else
+                    return resposta(false, usuario.Mensagens);
+            }
+            else
+                return resposta(false, "Ops.. Esse usuário já existe. Tente novamente");
+
+            return resposta(true, "Usuario cadastrado com sucesso!");
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult buscar(string email)
+        {
+            var usuario = repositorio.buscar(email);
+            return Ok(new { dados = usuario });
+        }
+
+    }
+}
---
+++ modulo-6-dotNet/AulaTerca/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Repositorios
+{
+   public class UsuarioRepositorio
+    {
+        private Contexto contexto = new Contexto();
+
+        public Usuario buscar(string email)
+        {
+            return contexto.Usuarios.Where(user => user.Email == email).FirstOrDefault();
+        }
+
+        public Usuario cadastrar(Usuario usuario)
+        {
+            contexto.Usuarios.Add(usuario);
+            contexto.SaveChanges();
+
+            return usuario;
+        }
+
+        public Usuario alterar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
