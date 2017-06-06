2a03b1aa6a5dacee76ee7f661ce4722cf7dfd605
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-06-06 09:52:51 -0300

    feat(imobiliaria api): implementar controller para usuarios

---
+++ modulo-6-dotNet/TrabalhoFinal/Back/Imobiliaria/Imobiliaria.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,81 @@
+﻿using Imobiliaria.Dominio.Entidades;
+using Imobiliaria.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace Imobiliaria.Api.Controllers
+{
+        [RoutePrefix("api/acessos")]
+        public class UsuarioController : ApiController
+        {
+            readonly UsuarioRepositorio _usuarioRepositorio;
+
+            public UsuarioController()
+            {
+                _usuarioRepositorio = new UsuarioRepositorio();
+            }
+
+            [HttpPost, Route("registrar")]
+            public HttpResponseMessage Registrar(Usuario usuario)
+            {
+                if (_usuarioRepositorio.Obter(usuario.Email) == null)
+                {
+
+                    if (usuario.Validar())
+                    {
+                        _usuarioRepositorio.Criar(usuario);
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.BadRequest, new { usuario.Mensagens });
+                    }
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Usuário já existe.");
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+
+            //[HttpPost, Route("resetarsenha")]
+            //public HttpResponseMessage ResetarSenha(string email)
+            //{
+            //    var usuario = _usuarioRepositorio.Obter(email);
+            //    if (usuario == null)
+            //        return Request.CreateResponse(HttpStatusCode.BadRequest, "Usuário não encontrado." );
+
+            //    var novaSenha = usuario.ResetarSenha();
+
+            //    if (usuario.Validar())
+            //    {
+            //        _usuarioRepositorio.Alterar(usuario);
+            //        // EmailService.Enviar(usuario.Email, "Crescer 2017-1", $"Olá! sua senha foi alterada para: {novaSenha}");
+            //    }
+            //    else
+            //        return Request.CreateResponse(HttpStatusCode.BadRequest, new { usuario.Mensagens });
+
+            //    return Request.CreateResponse(HttpStatusCode.OK);
+            //}
+
+            // Exige que o usuário se autentique
+
+            [HttpGet, Route("usuario")]
+            [BasicAuthorization(Roles = "Revisor")]
+            public IHttpActionResult Obter()
+            {
+                var usuarioDaSessao = _usuarioRepositorio.Obter(Thread.CurrentPrincipal.Identity.Name);
+
+                if (usuarioDaSessao == null)
+                {
+                    return BadRequest("Usuario não encontrado");
+                }
+                var usuario = new { Nome = usuarioDaSessao.Nome, Email = usuarioDaSessao.Email, Permissoes = usuarioDaSessao.Permissoes };
+                return Ok(new { dados = usuario });
+            }
+        }
+}
---
+++ modulo-6-dotNet/TrabalhoFinal/Back/Imobiliaria/Imobiliaria.Infraestrutura/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,59 @@
+﻿using Imobiliaria.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imobiliaria.Infraestrutura.Repositorios
+{
+    public class UsuarioRepositorio
+    {
+        //private Contexto contexto = new Contexto();
+
+        //public Usuario buscar(string email)
+        //{
+        //    return contexto.Usuarios.Where(user => user.Email == email).FirstOrDefault();
+        //}
+
+        //public Usuario cadastrar(Usuario usuario)
+        //{
+        //    contexto.Usuarios.Add(usuario);
+        //    contexto.SaveChanges();
+
+        //    return usuario;
+        //}
+
+        //public Usuario alterar()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        private Contexto contexto = new Contexto();
+
+        public void Criar(Usuario usuario)
+        {
+            contexto.Usuarios.Add(usuario);
+            contexto.SaveChanges();
+        }
+
+        //public void Alterar(Usuario usuario)
+        //{
+
+        //    contexto.Usuarios = usuario;
+        //}
+
+        //public IEnumerable<Usuario> Listar()
+        //{
+        //    return contexto.Select(u => u.Value);
+        //}
+
+        public Usuario Obter(string email)
+        {
+            return contexto.Usuarios.Where(u => u.Email == email)
+                .Include(a => a.Permissoes)
+                .FirstOrDefault();
+        }
+    }
+}
