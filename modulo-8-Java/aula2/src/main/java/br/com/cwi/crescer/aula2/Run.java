0d402db77777c637a321c0ace69d6826601865fe
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-06-21 17:30:57 -0300

    feat(java aula 2): adicionar chat

---
+++ modulo-8-Java/aula2/src/main/java/br/com/cwi/crescer/aula2/Run.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.aula2;
+
+import java.io.File;
+
+/**
+ *
+ * @author alana.weiss
+ */
+public class Run {
+    
+    final File file = new File(".");
+    final File[] list = new File(".").listFiles();
+   
+}
