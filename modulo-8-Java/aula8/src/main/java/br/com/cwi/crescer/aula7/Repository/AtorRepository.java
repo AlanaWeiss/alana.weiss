c1458c6f6ac0a93eb73968ba5546bf56a1477491
Alana Lange Weiss <alana.weiss@cwi.com.br>
2017-06-29 17:34:12 -0300

    .

---
+++ modulo-8-Java/aula8/src/main/java/br/com/cwi/crescer/aula7/Repository/AtorRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.aula7.Repository;
+
+import br.com.cwi.crescer.aula7.Entity.Ator;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author alana.weiss
+ */
+public interface AtorRepository extends CrudRepository<Ator, Long> {
+    
+}
